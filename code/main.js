b7906eca885b4b369cb0c3c9fef5009eb9d77fa5
Gitthuma <george.githuma@gmail.com>
2021-04-26 23:05:46 +0300

    Created a function called random(), which takes as parameters two generic bounds that the random number should be between, and returns the result.

--- code/main.js
+++ code/main.js
@@ -4,6 +4,12 @@ let para = document.createElement('p');
 
 // Add your code here
 
+/*Refactor the code that generates the random number into a separate function called random(), which takes as parameters two generic bounds that the random number should be between, and returns the result.*/
+
+function random(min, max) {
+    return num = Math.floor(Math.random() * (max - min));
+}
+
 // Don't edit the code below here!
 
 const section = document.querySelector('section');
